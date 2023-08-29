f1e9bf1fd1517ec5abf10222cd93279c0e0326c6
p6 <pavan@grahana.net>
2023-08-29 20:44:26 +0530

    improve helm pull

--- install-helm-operator.sh
+++ install-helm-operator.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-helm pull confluentinc/confluent-for-kubernetes --untar --untardir=cfk-operator --namespace cfk-operator
+
+# Check if directory already exists
+if [ -d "cfk-operator" ]; then
+  echo "Directory cfk-operator already exists."
+  read -p "If this is the first time you're setting this up, or if you want to get the latest Helm chart, click continue. Otherwise, the step to pull and untar the chart will be skipped. Continue? (y/n): " CONTINUE_PULL
+else
+  CONTINUE_PULL="y"
+fi
+
+# Conditionally pull and untar the Helm chart
+if [[ "$CONTINUE_PULL" == "y" || "$CONTINUE_PULL" == "Y" ]]; then
+  rm -Rf ./cfk-operator/
+  helm pull confluentinc/confluent-for-kubernetes --untar --untardir=cfk-operator
+else
+  echo "Skipping Helm chart pull and untar."
+fi
+
+# Perform Helm upgrade or install
 helm upgrade --install confluent-operator \
   confluentinc/confluent-for-kubernetes \
   --values ./cfk-operator/confluent-for-kubernetes/values.yaml \
   --namespace cfk-operator
+
